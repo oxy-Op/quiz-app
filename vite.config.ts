4f7c5a664920e147efc6e496da8469b06d209557
oxy-Op <mdrehaan7766@gmail.com>
2023-10-30 13:44:14 +0530

    vercel config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  base: '/projects/quiz-app/',
   plugins: [react()],
   resolve: {
     alias: {
